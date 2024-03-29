cfba5d068898d034098eac90db6fc4d4f17ebc74
Ayman <amarzouk@student.42abudhabi.ae>
2024-03-30 03:24:53 +0400

    ex01

--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ayman_marzouk <ayman_marzouk@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 03:09:23 by ayman_marzo       #+#    #+#             */
-/*   Updated: 2024/03/30 03:12:05 by ayman_marzo      ###   ########.fr       */
+/*   Updated: 2024/03/30 03:24:33 by ayman_marzo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(void)
 {
 	int	numOfZombies = 10;
-	std::string	zombieName = "42 Zombie";
+	std::string	zombieName = "Batman Zombie";
 
 	Zombie	*zombies = zombieHorde(numOfZombies, zombieName);
 
