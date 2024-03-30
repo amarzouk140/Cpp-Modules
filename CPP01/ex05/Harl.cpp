92006bf5ba6c4e1f8b8d8090b528efc1d0084af5
Ayman Marzouk <amarzouk@lab2r3s8.42abudhabi.ae>
2024-03-30 15:31:19 +0400

    ex05

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarzouk <amarzouk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/30 15:23:25 by amarzouk          #+#    #+#             */
+/*   Updated: 2024/03/30 15:23:32 by amarzouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void	Harl::debug (void)
+{
+	std::cout << "(DEBUG) I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::infos (void)
+{
+	std::cout << "(INFO) I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning (void)
+{
+	std::cout << "(WARNING) I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error (void)
+{
+	std::cout << "(ERROR) This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	void (Harl::*functions[4])(void) = {&Harl::debug, &Harl::infos, &Harl::warning, &Harl::error};
+
+	int levelNumber = getLevelNumber(level);
+
+	if (levelNumber < 4)
+		(this->*functions[levelNumber])();
+	else
+	 	std::cout << "Unknown complaint level: " << level << std::endl;
+}
+
+int	getLevelNumber(std::string level)
+{
+	int i;
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	
+	for (i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+			break;
+	}
+	return (i);
+}
---
+++ CPP01/ex05/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarzouk <amarzouk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/30 15:22:52 by amarzouk          #+#    #+#             */
+/*   Updated: 2024/03/30 15:26:16 by amarzouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("UNKNOWN");
+}
