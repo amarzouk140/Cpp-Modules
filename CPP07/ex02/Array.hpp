e0abc1f0dc923624165fdc517b1e616aeeb72076
Ayman Marzouk <amarzouk@lab3r3s8.42abudhabi.ae>
2024-05-22 14:37:56 +0400

    finish ex02

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,7 +6,84 @@
 /*   By: amarzouk <amarzouk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 12:12:58 by amarzouk          #+#    #+#             */
-/*   Updated: 2024/05/22 12:12:59 by amarzouk         ###   ########.fr       */
+/*   Updated: 2024/05/22 14:33:55 by amarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+#include <exception>
+#include <string>
+#include <stdexcept>
+#include <cstddef>
+
+template <typename T>
+class Array
+{
+    private:
+        T* _array;
+        unsigned int _size;
+    public:
+    Array() : _array(NULL), _size(0) {}
+    Array(unsigned int n) : _size(n) 
+        {
+        if (n == 0) 
+            _array = NULL;
+        else 
+            _array = new T[n]();
+        }
+    Array(const Array& other) : _size(other._size) 
+        {
+        if (_size == 0)
+            _array = NULL;
+        else 
+        {
+            _array = new T[_size]();
+            for (unsigned int i = 0; i < _size; ++i)
+                _array[i] = other._array[i];
+        }
+        }
+    T& operator[](unsigned int index) 
+    {
+        if (index >= _size)
+            throw std::out_of_range("Index out of bounds");
+        return _array[index];
+    }
+    Array& operator=(const Array& other) 
+        {
+        if (this != &other) 
+        {
+            delete[] _array;
+
+            _size = other._size;
+            if (_size == 0)
+                _array = NULL;
+            else 
+            {
+                _array = new T[_size]();
+                for (unsigned int i = 0; i < _size; ++i)
+                    _array[i] = other._array[i];
+            }
+        }
+            return *this;
+        }
+    ~Array() 
+        {
+        delete[] _array;
+        }        
+    const T& operator[](unsigned int index) const 
+        {
+        if (index >= _size)
+            throw std::out_of_range("Index out of bounds");
+        return _array[index];
+        }
+    
+    unsigned int size() const 
+    {
+        return _size;
+    }
+};
+
+#endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,7 +6,69 @@
 /*   By: amarzouk <amarzouk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 12:13:00 by amarzouk          #+#    #+#             */
-/*   Updated: 2024/05/22 12:13:01 by amarzouk         ###   ########.fr       */
+/*   Updated: 2024/05/22 14:37:11 by amarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+int main() 
+{
+    try 
+    {
+        /* Test with int data type */
+    
+        Array<int> intArray(5);
+        for (unsigned int i = 0; i < intArray.size(); ++i)
+            intArray[i] = i * 2;
+
+        std::cout << "intArray: ";
+        for (unsigned int i = 0; i < intArray.size(); ++i)
+            std::cout << intArray[i] << " ";
+        std::cout << std::endl;
+
+        /* Test with std::string data type */
+    
+        Array<std::string> strArray(3);
+        strArray[0] = "hello";
+        strArray[1] = "world";
+        strArray[2] = "!";
+
+        std::cout << "strArray: ";
+        for (unsigned int i = 0; i < strArray.size(); ++i)
+            std::cout << strArray[i] << " ";
+        std::cout << std::endl;
+
+        /* Test copy constructor and assignment operator */
+        
+        Array<int> copiedArray = intArray;
+        std::cout << "copiedArray: ";
+        for (unsigned int i = 0; i < copiedArray.size(); ++i)
+            std::cout << copiedArray[i] << " ";
+        std::cout << std::endl;
+
+        Array<std::string> assignedArray;
+        assignedArray = strArray;
+        std::cout << "assignedArray: ";
+        for (unsigned int i = 0; i < assignedArray.size(); ++i)
+            std::cout << assignedArray[i] << " ";
+        std::cout << std::endl;
+
+        /* Test out of bounds exception */
+        
+        try
+        {
+            std::cout << intArray[10] << std::endl;
+        } 
+        catch (const std::exception& e) 
+        {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    } 
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
