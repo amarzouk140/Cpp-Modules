e19dd6c30453a9fa408f61f32cc744a14836ec4e
Ayman <amarzouk@student.42abudhabi.ae>
2024-05-26 01:22:04 +0400

    finish ex02

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: ayman_marzouk <ayman_marzouk@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 23:08:26 by ayman_marzo       #+#    #+#             */
-/*   Updated: 2024/05/25 23:17:53 by ayman_marzo      ###   ########.fr       */
+/*   Updated: 2024/05/26 01:21:36 by ayman_marzo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,76 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <iostream>
 #include <stack>
 #include <deque>
-#include <list>
-#include <vector>
-#include <algorithm>
 #include <iterator>
-#include <stdexcept>
-#include <climits>
+#include <list>
+#include <iostream>
+#include <algorithm>
 
 
 template <typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack<T, Container>
+class MutantStack : public std::stack<T, Container> 
 {
-    public:
-        MutantStack() : std::stack<T, Container>() {}
-        MutantStack(const MutantStack &other) : std::stack<T, Container>(other) {}
-        virtual ~MutantStack() {}
-        MutantStack &operator=(const MutantStack &other) 
-        {
-            if (this != &other) 
-                std::stack<T, Container>::operator=(other);
-            return *this;
-        }
-        typedef typename std::stack<T, Container>::container_type::iterator iterator;
-        iterator begin() { return std::stack<T, Container>::c.begin(); }
-        iterator end() { return std::stack<T, Container>::c.end(); }
+public:
+
+    MutantStack() : std::stack<T, Container>() {}
+    MutantStack(const MutantStack &other) : std::stack<T, Container>(other) {}
+    virtual ~MutantStack() {}
+    MutantStack &operator=(const MutantStack &other) 
+    {
+        if (this != &other)
+            std::stack<T, Container>::operator=(other);
+        return *this;
+    }
+
+    // Typedefs for iterator types
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+    typedef typename Container::reverse_iterator reverse_iterator;
+    typedef typename Container::const_reverse_iterator const_reverse_iterator;
+    
+    // Iterator functions
+    iterator begin() 
+    {
+        return this->c.begin();
+    }
+    
+
+    iterator end() 
+    {
+        return this->c.end();
+    }
+
+    const_iterator begin() const 
+    {
+        return this->c.begin();
+    }
+
+    const_iterator end() const 
+    {
+        return this->c.end();
+    }
+
+    reverse_iterator rbegin() 
+    {
+        return this->c.rbegin();
+    }
+
+    reverse_iterator rend() 
+    {
+        return this->c.rend();
+    }
+
+    const_reverse_iterator rbegin() const 
+    {
+        return this->c.rbegin();
+    }
+
+    const_reverse_iterator rend() const 
+    {
+        return this->c.rend();
+    }
 };
 
 #endif
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,26 +6,78 @@
 /*   By: ayman_marzouk <ayman_marzouk@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 23:08:31 by ayman_marzo       #+#    #+#             */
-/*   Updated: 2024/05/25 23:11:17 by ayman_marzo      ###   ########.fr       */
+/*   Updated: 2024/05/26 01:15:58 by ayman_marzo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-int main()
+int main() 
 {
-MutantStack<int> mstack;
-mstack.push(5);
-mstack.push(17);
-std::cout << mstack.top() << std::endl;
-mstack.pop();
-std::cout << mstack.size() << std::endl;
-mstack.push(3); mstack.push(5); mstack.push(737); //[...] mstack.push(0);
-MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
-++it;
---it;
-while (it != ite) {
-    std::cout << *it << std::endl;
-++it; }
-std::stack<int> s(mstack); return 0;
+    // Test with MutantStack
+    std::cout << "subject test for MutantStack:" << std::endl;
+    
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << "Top element: " << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << "Size after pop: " << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+
+    std::cout << "MutantStack elements:" << std::endl;
+    while (it != ite) 
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+
+    std::stack<int> s(mstack);
+
+    // Test with std::list
+    std::cout << "\nTesting std::list:" << std::endl;
+    std::list<int> lstack;
+
+    lstack.push_back(5);
+    lstack.push_back(17);
+
+    std::cout << "Top element: " << lstack.back() << std::endl;
+
+    lstack.pop_back();
+
+    std::cout << "Size after pop: " << lstack.size() << std::endl;
+
+    lstack.push_back(3);
+    lstack.push_back(5);
+    lstack.push_back(737);
+    lstack.push_back(0);
+
+    std::list<int>::iterator lit = lstack.begin();
+    std::list<int>::iterator lite = lstack.end();
+
+    ++lit;
+    --lit;
+
+    std::cout << "std::list elements:" << std::endl;
+    while (lit != lite) 
+    {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+
+    return 0;
 }
